50845bdb08a8b006279adebdbf75041df3fc0f88
Kenneth <kenneth.vangijsel@oak3.be>
2018-08-16 13:11:44 +0200

    Add Maven

--- src/be/oak3/model/Parfum.java
+++
@@ -1,9 +0,0 @@
-package be.oak3.model;
-
-public class Parfum extends Product {
-
-    public Parfum(int number, String brandName, String name, int volume,
-                  double price) {
-        super(number, brandName, name, volume, price);
-    }
-}
